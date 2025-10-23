eb6f7860c44c51f97af83810a1fd381d8a5485f4
simondevz <obidaberechukwu@gmail.com>
2025-10-23 03:52:00 +0100

    update: added new resources to resource file

--- src/subgraph_pipeline/defs/resources.py
+++ src/subgraph_pipeline/defs/resources.py
@@ -1,5 +1,9 @@
 import dagster as dg
 
+from database.database_client import DatabaseClient
+from database.entity_manager import EntityManager
+from database.event_loader import EventLoader
+from utils.event_transformers import EventTransformer
 from utils.query_builder import SubgraphQueryBuilder
 from utils.subgraph_client import SubgraphClient
 
@@ -13,5 +17,13 @@ def resources():
                 endpoint=dg.EnvVar("SUBGRAPH_ENDPOINT"),
                 api_key=dg.EnvVar("SUBGRAPH_API_KEY"),
             ),
+            "db_client": DatabaseClient(
+                connection_string=dg.EnvVar("POSTGRES_CONNECTION_STRING"),
+                pool_size=5,
+                max_overflow=10,
+            ),
+            "entity_manager": EntityManager(),
+            "event_loader": EventLoader(),
+            "transformer": EventTransformer(),
         }
     )
